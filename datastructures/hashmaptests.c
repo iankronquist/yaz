86ff4dfe33599e210b293a035702a37589e02f06
Ian Kronquist <iankronquist@teleport.com>
2014-07-05 12:40:17 -0700

    Add test for hashmap_addVal

--- datastructures/hashmaptests.c
+++ datastructures/hashmaptests.c
@@ -2,7 +2,7 @@
 #include "hashmap.h"
 
 void test_hashmap_new();
-void test_hashmap_delete();
+void test_hashmap_addVal();
 #define EXPECT_EQ(a, b) printf("Test %s == %s ", (#a), (#b));\
 if ((a) == (b)) {\
     puts(" passed");\
@@ -22,7 +22,8 @@ else {\
 int main()
 {
     test_hashmap_new();
-    
+    test_hashmap_addVal();
+    return 0;
 }
 
 void test_hashmap_new()
@@ -33,4 +34,15 @@ void test_hashmap_new()
     {
         EXPECT_EQ(map->_buckets[i], 0);    
     }
+}
+
+void test_hashmap_addVal()
+{
+    hashmap* map = hashmap_new();
+    hashmap_addVal(map, "a", 0);
+    struct _hashnode* first = map->_buckets[1];
+    EXPECT_EQ(strcmp(first->key, "a"), 0);
+    EXPECT_EQ(first->val, 0);
+    EXPECT_EQ(first->next, NULL);
+    EXPECT_EQ(map->numElems, 1);
 }
