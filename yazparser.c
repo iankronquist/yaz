f397f34378440a88812ed8c4540106d6e9aa7fe8
Ian Kronquist <iankronquist@teleport.com>
2014-07-27 19:26:55 -0700

    Rewrite of the parser

--- yazparser.c
+++ yazparser.c
@@ -1,2 +1,105 @@
+#include "yazparser.h"
 
+const char* USEAGE = "Useage:\nyaz filename\n";
+void read_file_by_lines(char* filename);
+void extend_children_list(struct treenode* parent,
+        struct treenode* newchild);
+struct treenode* add_ast(struct treenode* parent);
+void error(char* message, char* snippet);
 
+/*
+   Program return values:
+ * 0: exited successfully
+ * 1: invalid arguments
+ * 2: file read error
+ * 3: invalid syntax
+ */
+int main(int argc, char** argv)
+{
+    puts("0");
+    printf("i%i\n", argc);
+    if(argc == 1)
+    {
+        puts("0.1");
+        printf("%s", USEAGE);
+        puts("0.2");
+        return 1;
+    }
+    puts("1");
+    parseFile(argv[1]);
+}
+
+void parseFile(char* fileName) {
+    FILE* file = fopen(fileName, "r");
+    char* line = NULL;
+    size_t line_capacity = 0;
+    ssize_t line_length = 0;
+    while ((line_length = getline(&line, &line_capacity, file)) > 0) {
+        tree* ast = parseStatement(line);
+        tree_print(ast);
+        //tree_delete(ast);
+    }
+}
+
+tree* parseStatement(char* line) {
+    if(line[0] == '/' && line[1] == '/')
+        return NULL;
+    tree* astRoot = (tree*) malloc(sizeof(struct treenode));
+    tree* parent = astRoot;
+    astRoot->num_children = 0;
+    astRoot->children = NULL;
+    char* sep = " ";
+    char* word;
+    char* brk;
+    word = strtok_r(line, sep, &brk);
+    printf("word %s\n", word);
+    assert(word[0] == '(');
+    word = strtok_r(NULL, sep, &brk);
+    printf("word %s\n", word);
+    astRoot->symbol = word;
+    return parseHelper(astRoot, NULL, line, sep, brk);
+}
+
+tree* parseHelper(tree* parent, tree* grandParent, char* line, char* sep, char* brk) {
+    char* word = strtok_r(NULL, sep, &brk);
+    printf("word %s\n", word);
+    if (word == NULL) {
+        return NULL;
+    }
+    if (word[0] == '(') {
+        char* nextWord = strtok_r(NULL, sep, &brk);
+        tree* ggp = grandParent;
+        grandParent = parent;
+        puts("opend again");
+        parent = addChild(parent, nextWord);
+        parseHelper(parent, grandParent, line, sep, brk);
+        parseHelper(grandParent, ggp, line, sep, brk);
+        return parent;
+    } else if (word[0] == ')') {
+        return parent;
+    } else {
+        // word in table etc. etc.
+        addChild(parent, word);
+        parseHelper(parent, grandParent, line, sep, brk);
+        return parent;
+    }
+    assert(0);
+    //silence errors
+    return NULL;
+}
+
+tree* addChild(tree* parent, char* token) {
+    tree* newElem = (tree*)malloc(sizeof(tree*));
+    newElem->symbol = token;
+    newElem->children = NULL;
+    newElem->num_children = 0;
+    parent->children = realloc(parent->children, (parent->num_children + 1) * sizeof(tree*));
+    parent->children[parent->num_children] = newElem;
+    parent->num_children++;
+    return newElem;
+}
+
+void error(char* message, char* snippet) {
+    printf("%s %s\n", message, snippet);
+    exit(-1);
+}
--- yazparser.h
+++ yazparser.h
@@ -1,4 +1,44 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
 #include "datastructures/hashmap.h"
+#include "datastructures/tree.h"
 
-hashmap symbol_table;
+hashmap* symbol_table;
+hashmap* function_table;
+void read_file_by_lines(char* filename);
+tree* build_ast(char* line);
+void add_token_to_tree(char* token);
+void execute_statement(tree* statement);
+void execute_function(void* arguments, size_t num_args, char* function_symbol);
+void cleanup();
+void exit_with_message(char* message);
+void parseFile(char* fileName);
+tree* parseStatement(char* line);
+void addTokenToTree(tree* astParent, char* token, size_t tokenLength);
+void addSubTreeToTree(tree* astParent, tree* subTree);
+tree* parseStatement(char* line);
+tree* parseHelper(tree* parent, tree* grandParent, char* line, char* sep, char* brk);
+tree* addChild(tree* parent, char* token);
 
+
+struct function {
+    size_t numargs;
+    char** argnames;
+};
+
+struct variable {
+    int type;
+    size_t size;
+};
+
+union tokenVal {
+    struct function func;
+    struct variable var;
+};
+
+struct token {
+    bool isFunction;
+    char* name;
+    union tokenVal;
+};
