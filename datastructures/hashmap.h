705f2fafc523657dab0acdf14e8dad5e20a62ce2
Ian Kronquist <iankronquist@teleport.com>
2014-08-04 20:59:28 -0700

    Add comments to hashmap

--- datastructures/hashmap.h
+++ datastructures/hashmap.h
@@ -15,35 +15,35 @@ struct hashmap {
     size_t numElems;
 };
 
-//TODO: determine whether this is a reasonable number
+// TODO: determine whether this is a reasonable number
 #define HASHMAPMAXLOAD .75
 
 typedef struct hashmap hashmap;
 
-//Creates a new hashmap
+/** Creates a new hashmap */
 hashmap* hashmap_new();
-//Creates a new hashmap of arbitrary size
+/** Creates a new hashmap of arbitrary size */
 struct _hashnode** _hashtable_init(size_t size);
-//deletes a hashmap
+/** deletes a hashmap */
 void hashmap_delete(hashmap* map);
-//Adds a key value pair to the hashmap
+/** Adds a key value pair to the hashmap */
 void hashmap_addVal(hashmap* map, char* key, void* val);
-//Returns true if the key is in the hashmap, otherwise false
+/** Returns true if the key is in the hashmap, otherwise false */
 bool hashmap_hasKey(hashmap* map, char* key);
-//Removes a key value pair from the hashmap
+/** Removes a key value pair from the hashmap */
 void hashmap_removeKey(hashmap* map, char* key);
-//Given a key, get a value. If the value does not exist, return NULL
+/** Given a key, get a value. If the value does not exist, return NULL */
 void* hashmap_getVal(hashmap* map, char* key);
-//Given a key and a new value set the key's value to the new value
-//NOTE: The original value will be freed
+/** Given a key and a new value set the key's value to the new value */
+/** NOTE: The original value will be freed */
 void hashmap_setVal(hashmap* map, char* key, void* val);
-//Returns the number of elements in the hash map
+/** Returns the number of elements in the hash map */
 size_t hashmap_numElems(hashmap* map); 
-//Return the bucket where the key should reside
+/** Return the bucket where the key should reside */
 size_t _hash(size_t mapsize, char* key);
-//Return the load on the hashmap
+/** Return the load on the hashmap */
 float _hashmap_load(hashmap* map);
-//Resize the hashmap
+/** Resize the hashmap */
 void _hashmap_resize(hashmap* map, size_t newSize);
 void _hashnodetable_add(struct _hashnode** nodetable,
     struct _hashnode* newNode, size_t index);
