406408f7b8898509975107c4a6472b287793649c
Ian Kronquist <iankronquist@teleport.com>
2015-02-19 23:02:44 -0800

    Add string comparison tests

--- tests.h
+++ tests.h
@@ -1,6 +1,7 @@
 #ifndef TESTS_H
 #define TESTS_H
 #include <stdio.h>
+#include <string.h>
 
 #define RED "\x1B[0;31m"
 #define GREEN "\x1B[0;32m"
@@ -14,6 +15,21 @@ else {\
     printf(" %sFailed%s\n", RED, NOCOLOR);\
 }
 
+#define EXPECT_EQ_STR(a, b) printf("Test %s == %s ", (#a), (#b));\
+if (strcmp((a), (b)) == 0) {\
+    printf(" %sPassed%s\n", GREEN, NOCOLOR);\
+}\
+else {\
+    printf(" %sFailed%s\n", RED, NOCOLOR);\
+}
+
+#define EXPECT_NEQ_STR(a, b) printf("Test %s == %s ", (#a), (#b));\
+if (strcmp((a), (b)) != 0) {\
+    printf(" %sPassed%s\n", GREEN, NOCOLOR);\
+}\
+else {\
+    printf(" %sFailed%s\n", RED, NOCOLOR);\
+}
 #define EXPECT_NEQ(a, b) printf("Test %s != %s ", (#a), (#b));\
 if ((a) != (b)) {\
     printf(" %sPassed%s\n", GREEN, NOCOLOR);\
