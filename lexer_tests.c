c7c21731db7198b5f5b7dc3ff0516f5f7a955689
Ian Kronquist <iankronquist@teleport.com>
2015-02-19 23:01:02 -0800

    Start fixing the lexer tests

--- lexer_tests.c
+++ lexer_tests.c
@@ -1,4 +1,5 @@
 #include "lexer.h"
+#include "token.h"
 #include "tests.h"
 
 
@@ -12,24 +13,14 @@ void exit(int status) {
 void test_make_token_list();
 void test_destroy_token_list();
 void test_append_token_list();
-void test_gen_tok();
-void test_is_identifier();
-void test_is_num();
-void test_is_string();
-void test_is_extern();
-void test_is_func();
+void test_get_tok();
 
 
 int main() {
     test_make_token_list();
     test_append_token_list();
     test_destroy_token_list();
-    test_gen_tok();
-    test_is_identifier();
-    test_is_num();
-    test_is_string();
-    test_is_extern();
-    test_is_func();
+    test_get_tok();
     return 0;
 }
 
@@ -66,101 +57,45 @@ void test_destroy_token_list() {
     destroy_token_list(tkl);
 }
 
-void test_gen_tok(char* new_token) {
-    struct token *received;
+void test_get_tok(char* new_token) {
+    struct token_list *received;
     char *tok;
 
     tok = "";
-    received = gen_tok(tok);
+    received = get_tok(tok, strlen(tok));
     EXPECT_EQ(received, NULL);
 
     tok = "1.23";
-    received = gen_tok(tok);
+    received = get_tok(tok, strlen(tok));
     EXPECT_EQ(received->type, tok_number);
     EXPECT_EQ(received->value.dbl, 1.23);
     EXPECT_EQ(received->next_token, NULL);
 
     tok = "\"abc def\"";
-    received = gen_tok(tok);
+    received = get_tok(tok, strlen(tok));
     EXPECT_EQ(received->type, tok_string);
-    EXPECT_EQ(received->value.dbl, 1.23);
+    EXPECT_EQ_STR(received->value.string, "abc def");
     EXPECT_EQ(received->next_token, NULL);
 
     tok = "func";
-    received = gen_tok(tok);
+    received = get_tok(tok, strlen(tok));
     EXPECT_EQ(received->type, tok_def);
     EXPECT_EQ(received->value.dbl, 1.23);
     EXPECT_EQ(received->next_token, NULL);
 
     tok = "abc123";
-    received = gen_tok(tok);
+    received = get_tok(tok, strlen(tok));
     EXPECT_EQ(received->type, tok_identifier);
     EXPECT_EQ(received->value.string, "abc123");
     EXPECT_EQ(received->next_token, NULL);
 
     tok = "+";
-    received = gen_tok(tok);
+    received = get_tok(tok, strlen(tok));
     EXPECT_EQ(received->type, tok_punc);
     EXPECT_EQ(received->value.string, "+");
     EXPECT_EQ(received->next_token, NULL);
 
     tok = "..";
-    received = gen_tok(tok);
+    received = get_tok(tok, strlen(tok));
     EXPECT_EQ(exit_called_with, -1);
-}
-
-void test_is_identifier() {
-    char *tok;
-    tok = "abc";
-    EXPECT_EQ(is_identifier(tok), true);
-    tok = "a_b-c";
-    EXPECT_EQ(is_identifier(tok), true);
-    tok = "abc123";
-    EXPECT_EQ(is_identifier(tok), true);
-    tok = "123abc";
-    EXPECT_EQ(is_identifier(tok), true);
-    tok = "abc+123";
-    EXPECT_EQ(is_identifier(tok), false);
-}
-
-void test_is_extern() {
-    char *tok;
-    tok = "extern";
-    EXPECT_EQ(is_extern(tok), true);
-    tok = "abc";
-    EXPECT_EQ(is_extern(tok), false);
-}
-
-void test_is_func(char *test_str) {
-    char *tok;
-    tok = "func";
-    EXPECT_EQ(is_func(tok), true);
-    tok = "abc";
-    EXPECT_EQ(is_func(tok), false);
-}
-
-void test_is_num() {
-    char *tok;
-    tok = "1.2.3";
-    EXPECT_EQ(is_num(tok), false);
-    tok = "1.23";
-    EXPECT_EQ(is_num(tok), true);
-    tok = ".23";
-    EXPECT_EQ(is_num(tok), true);
-    tok = "123";
-    EXPECT_EQ(is_num(tok), true);
-    tok = "0x123";
-    EXPECT_EQ(is_num(tok), true);
-}
-
-void test_is_string() {
-    char *tok;
-    tok = "\"";
-    EXPECT_EQ(is_string(tok), false);
-    tok = "\"\"";
-    EXPECT_EQ(is_string(tok), true);
-    tok = "\"\\\"\\\"";
-    EXPECT_EQ(is_string(tok), false);
-    tok = "\"abc 123\"";
-    EXPECT_EQ(is_num(tok), true);
 }
