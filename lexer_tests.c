811e22baf1aa525293f28264b765c6a8c3b7cc18
Ian Kronquist <iankronquist@teleport.com>
2015-02-21 15:24:36 -0800

    Fix memory leak in tests

--- lexer_tests.c
+++ lexer_tests.c
@@ -12,14 +12,15 @@ int main() {
 void test_get_tok(char* new_token) {
     struct token_list *received;
     char *tok;
-
     tok = "\n";
     received = get_tok(tok, strlen(tok));
     EXPECT_EQ(received->head, NULL);
+    destroy_token_list(received);
 
     tok = "# asdf\n";
     received = get_tok(tok, strlen(tok));
     EXPECT_EQ(received->head, NULL);
+    destroy_token_list(received);
 
     tok = "1.23   \n";
     received = get_tok(tok, strlen(tok));
@@ -27,6 +28,7 @@ void test_get_tok(char* new_token) {
     EXPECT_EQ(received->head->type, tok_dbl);
     EXPECT_EQ(received->head->value.dbl, 1.23);
     EXPECT_EQ(received->head->next_token, NULL);
+    destroy_token_list(received);
 
     tok = "\"abc def\"\n";
     received = get_tok(tok, strlen(tok));
@@ -34,6 +36,7 @@ void test_get_tok(char* new_token) {
     EXPECT_EQ(received->head->type, tok_string);
     EXPECT_EQ_STR(received->head->value.string, "abc def");
     EXPECT_EQ(received->head->next_token, NULL);
+    destroy_token_list(received);
 
     tok = "def\n";
     received = get_tok(tok, strlen(tok));
@@ -41,6 +44,7 @@ void test_get_tok(char* new_token) {
     EXPECT_EQ(received->head->type, tok_def);
     EXPECT_EQ(received->head->value.string, NULL);
     EXPECT_EQ(received->head->next_token, NULL);
+    destroy_token_list(received);
 
     tok = "abc123\n";
     received = get_tok(tok, strlen(tok));
@@ -48,6 +52,7 @@ void test_get_tok(char* new_token) {
     EXPECT_EQ(received->head->type, tok_identifier);
     EXPECT_EQ_STR(received->head->value.string, "abc123");
     EXPECT_EQ(received->head->next_token, NULL);
+    destroy_token_list(received);
 
     tok = "+\n";
     received = get_tok(tok, strlen(tok));
@@ -55,6 +60,7 @@ void test_get_tok(char* new_token) {
     EXPECT_EQ(received->head->type, tok_punc);
     EXPECT_EQ_STR(received->head->value.string, "+");
     EXPECT_EQ(received->head->next_token, NULL);
+    destroy_token_list(received);
 
     tok = "!=\n";
     received = get_tok(tok, strlen(tok));
@@ -62,6 +68,5 @@ void test_get_tok(char* new_token) {
     EXPECT_EQ(received->head->type, tok_punc);
     EXPECT_EQ_STR(received->head->value.string, "!=");
     EXPECT_EQ(received->head->next_token, NULL);
-
     destroy_token_list(received);
 }
