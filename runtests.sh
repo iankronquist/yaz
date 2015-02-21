8bfa222682789ef98bee6e3b2e8dead24d3fd891
Ian Kronquist <iankronquist@teleport.com>
2015-02-21 12:19:07 -0800

    Fix syntax errors; Echo errors to stderr

--- runtests.sh
+++ runtests.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+make tests
+
 for file in $(ls tests/bin); do
+	echo "Testing " $file
 	if valgrind --error-exitcode=0 tests/bin/$file $TEST_ARGS; then
-		echo "Error: $file leaking memory"
+		echo "Error:" $file "leaking memory" 1>&2
 	fi
 	tests/bin/$file
 done
 
 for file in $(ls build/bin); do
+	echo "Testing " $file
 	if valgrind --error-exitcode=0 $file $ARGS; then 
 		echo "Error: leaking memory"
 	fi
