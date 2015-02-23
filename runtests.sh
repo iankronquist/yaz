a5f433152e0f856112ac5eb984d1326a42f669e0
Ian Kronquist <iankronquist@teleport.com>
2015-02-22 19:37:35 -0800

    Replace ls with find in test runner script

--- runtests.sh
+++ runtests.sh
@@ -2,15 +2,15 @@
 
 make tests
 
-for file in $(ls tests/bin); do
+for file in $(find tests/bin -type f -maxdepth 1); do
 	echo "Testing " $file
-	if valgrind --error-exitcode=0 tests/bin/$file $TEST_ARGS; then
+	if valgrind --error-exitcode=0 $file $TEST_ARGS; then
 		echo "Error:" $file "leaking memory" 1>&2
 	fi
 	tests/bin/$file
 done
 
-for file in $(ls build/bin); do
+for file in $(find build/bin -type f -maxdepth 1); do
 	echo "Testing " $file
 	if valgrind --error-exitcode=0 $file $ARGS; then 
 		echo "Error: leaking memory"
