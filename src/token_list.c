c02cad04637e5ee61b0e4d7d57fd32ff6b139845
Ian Kronquist <iankronquist@teleport.com>
2015-03-01 13:48:54 -0800

    Fix bug in token list
    Peek should peek at the head value, not the one following the head.

--- src/token_list.c
+++ src/token_list.c
@@ -57,7 +57,7 @@ struct token* pop_token_list(struct token_list* tk_list) {
 struct token* peek_token_list(struct token_list* tk_list) {
     struct token *cur = tk_list->head;
     if (cur != NULL) {
-        return tk_list->head->next_token;
+        return cur;
     } else {
         return NULL;
     }
